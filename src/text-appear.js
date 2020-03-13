472e3ebcceeb6bcb65ff6d465ff68082e878345d
Jason Farrell <jason@useallfive.com>
2020-03-13 14:13:05 -0700

    Simplify text line animation logic. - Handles nested html - Shouldn't affect layout

--- src/text-appear.js
+++ src/text-appear.js
@@ -1,4 +1,3 @@
-import { Box } from 'rebass'
 import PropTypes from 'prop-types'
 import React, { useEffect, useState, useRef, Component } from 'react'
 import { TweenMax } from 'gsap'
@@ -42,37 +41,42 @@ export class TextAppear extends Component {
         console.log(`Drizzle Warning: type="text" must only contain one child node. Contains: ${childNodeCount}:`)
         console.log(this.groupRef.current.innerHTML)
       }
-      wrapLines(el)
-      const lines = el.querySelectorAll('.ani-line')
+      let lineCount = wrapLines(el)
+      const lines = el.querySelectorAll('.drizzle-word')
       const speed = this.props.speed / lines.length + 1
 
       const complete = (i) => {
-        if (i === lines.length - 1) {
+        if (i === lineCount - 1) {
           el.innerHTML = originalContent
-          // this.completeHandler()
+          //   this.completeHandler()
           this.setState({ complete: true })
         }
       }
+      el.querySelectorAll('a').forEach((link) => {
+        link.style.textDecoration = 'none'
+      })
 
-      lines.forEach((item, index) => {
-        const $group = item.querySelector('.ani-line-group')
-        item.style.display = 'block'
-        $group.style.display = 'inline-block'
+      for (var i = 0; i < lineCount; i++) {
+        const lineWords = el.querySelectorAll(`.drizz-word-line-${i}`)
+        lineWords.forEach((lw) => {
+          lw.style.position = 'relative'
+          lw.style.display = 'inline-block'
+        })
         TweenMax.fromTo(
-          $group,
+          lineWords,
           speed,
           { y: this.props.distance, opacity: 0 },
           {
             y: 0,
             opacity: 1,
-            delay: this.props.delay + index * this.context.textLineStaggerSpeed,
+            delay: this.props.delay + i * this.context.textLineStaggerSpeed,
             onComplete: complete,
-            onCompleteParams: [index],
+            onCompleteParams: [i],
             ease: this.props.ease,
             clearProps: 'all',
           }
         )
-      })
+      }
       this.setState({ hasShown: true })
     }
   }
@@ -108,85 +112,22 @@ TextAppear.propTypes = {
 TextAppear.contextType = DrizzleContext
 
 function wrapLines($container) {
-  // get the text from the conatiner
-  // console.log($container.innerHTML)
-  $container.innerHTML = $container.innerHTML.replace(/<br>/g, '\n')
-  // console.log($container.innerHTML)
-
-  var text = $container.textContent
-
-  // split the text into words
-  var words = text.split(' ')
-
-  var parsedWords = []
-  words.forEach((word) => {
-    if (word.indexOf('-') !== -1) {
-      var hyphenatedWords = word.split('-')
-      hyphenatedWords.forEach((hw, i) => {
-        let wordUpdated = hw
-        if (i !== hyphenatedWords.length - 1) {
-          wordUpdated += '-'
-        }
-        parsedWords.push(wordUpdated)
-      })
-    } else {
-      parsedWords.push(word)
+  // wraps words in spans, preserving html
+  $container.innerHTML = $container.innerHTML.replace(
+    /(^|<\/?[^>]+>|\s+)([^\s<]+)/g,
+    '$1<span class="drizzle-word">$2</span>'
+  )
+  let lineIndex = -1
+  let lineTop = null
+  let wordEls = $container.querySelectorAll('.drizzle-word')
+  console.log(wordEls)
+  wordEls.forEach((word, index) => {
+    if (word.offsetTop !== lineTop) {
+      lineTop = word.offsetTop
+      lineIndex++
     }
+    word.classList.add(`drizz-word-line-${lineIndex}`)
   })
-
-  // wrap each word in a span and add it to a tmp
-  var tmp = ''
-  parsedWords.forEach((word) => {
-    let spacing = word.indexOf('-') !== -1 ? '' : ' '
-    if (word.indexOf('\n') === -1) {
-      tmp += `<span>${word}${spacing}</span>`
-    } else {
-      const breakWord = word.split('\n')
-      breakWord.forEach((w, i) => {
-        let spacing = w.indexOf('-') !== -1 ? '' : ' '
-        const selector = i > 0 ? ' class="ani-break"' : ''
-        tmp += `<span${selector}>${w}${spacing}</span>`
-      })
-    }
-  })
-
-  // remove the text from the container, and replace it with the wrapped words
-  $container.innerHTML = tmp
-
-  // prepare the offset variable and tmp
-  var top = null
-  var wordSpans = $container.querySelectorAll('span')
-  var len = wordSpans.length
-  wordSpans.forEach((word, index) => {
-    // if this is the first iteration
-    if (top === null) {
-      // set the top
-      top = word.offsetTop
-      // open the first line
-      tmp = '<span class="ani-line"><span class="ani-line-group">'
-    }
-
-    // if this is a new line (top is bigger then the previous word)
-    if (top < word.offsetTop || word.classList.contains('ani-break')) {
-      // close the previous line and start a new one
-      tmp += '</span></span><span class="ani-line"><span class="ani-line-group">'
-      // change the top
-      top = word.offsetTop
-    }
-    let spacing = word.innerText.indexOf('-') !== -1 ? '' : ' '
-    // add the content of the word node + a space
-    tmp += word.innerText
-    if (len - 1 > index) {
-      tmp += spacing
-    }
-  })
-  // close the last line
-  tmp += '</span></span>'
-  // remove the content of the conatiner, and replace it with the wrapped lines
-  $container.innerHTML = tmp
-  $container.querySelectorAll('.ani-line').forEach((item) => {
-    if (item.innerText === '' || item.innerText === ' ') {
-      $container.removeChild(item)
-    }
-  })
+  const lineCount = lineIndex + 1
+  return lineCount
 }
